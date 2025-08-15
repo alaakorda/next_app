be4011277331711522d0f273eda77be40253fb0b
Alaa Zakaria <alaazakaria436@gmail.com>
2025-08-15 17:33:43 +0300

    update

--- src/app/Efraim/page.tsx
+++ src/app/Efraim/page.tsx
@@ -20,7 +20,7 @@ export default function Page1() {
         <div className="p-4 text-center transition-opacity duration-500 group-hover:opacity-100 opacity-80">
           <h2 className="text-xl font-semibold">Efraim</h2>
           <p className="text-gray-500 mt-2">
-           Efraim is not just a brilliant and creative mind, but also a loyal and supportive friend. He’s the kind of person who’s always there when you need him, bringing both kindness and clever ideas to every situation. His talent and creativity stand out in everything he does, and his dedication makes him truly special to work with and to have as a friend.
+          Efraim is a very smart and creative person, and he’s also a true friend. Whenever I need help or someone to talk to, he’s always there. He listens, gives ideas, and somehow makes things better. He has this way of making people feel comfortable and supported. And okay… I have to admit, sometimes he can be a little evil — but in the best way possible. Everything he does shows his talent and his kind heart. I’m really lucky to work with him and to have him as my best and most loyal friend.
           </p>
         </div>
       </div>
